ecf61ec3d36a6c46f5c23bd9c370096a0d39c013
AmitSheth9 <amit.sheth9@gmail.com>
2022-02-14 18:37:46 -0600

    wrote save and get but test not passing

--- __tests__/simple-db.test.js
+++ __tests__/simple-db.test.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-console */
 const fs = require('fs/promises');
 const path = require('path');
+const SimpleDb = require('../lib/simple-db');
 
 const { CI, HOME } = process.env;
 const BASE_DIR = CI ? HOME : __dirname;
@@ -11,9 +13,29 @@ describe('simple database', () => {
     await fs.rm(TEST_DIR, { force: true, recursive: true });
     await fs.mkdir(TEST_DIR, { recursive: true });
   });
+  it.skip('takes an object and creates a file and saves it', async() => {
+    
 
-  it('needs a first test...', async () => {
 
   });
+  it('gets a file by id and returns deserialized object contents', async () => {
+    const db = new SimpleDb(TEST_DIR);
+    const testObj = {
+      contents: 'file contents'
+    };
+    await db.save(testObj);
+    console.log('testobj', testObj);
+    //console.log('testnewFile', newFile);
+    //console.log('this', this);
+    console.log('id', testObj.id);
+    const stringTestObj = JSON.stringify(testObj);
+    const newObject = await db.get(stringTestObj.id);
+    console.log('testnewObj', newObject);
+    expect(newObject).toEqual({ 
+      id: expect.any(String),
+      contents: 'file contents' });      
+  });
+  
+  
 
 });
